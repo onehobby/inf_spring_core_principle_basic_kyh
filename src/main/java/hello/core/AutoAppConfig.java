d8ca71706669bc4d716e4efe10c33b6fb0695933
yoo <zcx52111@naver.com>
2021-10-03 14:03:18 +0900

    의존관계 자동 주입

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -12,17 +12,17 @@ import static org.springframework.context.annotation.ComponentScan.*;
 @Configuration
 @ComponentScan(
 //      ComponentScan의 시작패키지를 정할 수 있다.
-        basePackages = "hello.core.member",
+//        basePackages = "hello.core.member",
 //      지정한 클래스의 패키지를 탐색 시작 위치로 지정한다.
-        basePackageClasses = AutoAppConfig.class,
+//        basePackageClasses = AutoAppConfig.class,
 //        ComponentScan은 @Component이 붙은 클래스들을 읽어서 ApplicationContext가 스프링의 bean으로 등록한다.
 //        ComponentScan의 excludeFilters설정은 제외할 클래스를 설정할 수 있다.
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
+    /*@Bean
     MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+        return null;
+    }*/
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,16 +1,20 @@
 package hello.core.order;
 
+import hello.core.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.discount.fixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @Component
+/*@RequiredArgsConstructor  lombok의 @RequiredArgsConstructor :: final 지시어가 붙은 변수들을 매개변수로 받는 생성자를 자동으로 만들어줌 */
 public class OrderServiceImpl implements OrderService {
     /*
     * SRP(Single Responsibility Principle) 위배, 마치 배우가 다른배우를 섭외하는 꼴(?)
@@ -27,8 +31,13 @@ public class OrderServiceImpl implements OrderService {
     /*생성자를 통해서 외부에서 memberRepository에 구현체를 주입 ==> 생성자주입
     , DIP 원칙을 위배하지 않음, 추상화에만 의존하고 구체화에는 의존하지 않는다.
     */
-    /*@Autowired*/
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    /*@Autowired :: 생성자가 하나 일 경우 Autowired를 생략할 수 있다.*/
+    /*lombok의 @RequiredArgsConstructor를 통해 final 지시어가 붙은 변수들을 매개변수로 받는 생성자를 자동으로 만들어줌*/
+    /*spring bean으로 등록된 DiscountPolicy 구현체가 2개이므로 UnsatisfiedDependencyException발생,
+    * 이 때 매개변수에 원하는 구현체의 이름을 매칭시켜주면 된다. ex)rateDiscountPolicy
+    * */
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, /*@Qualifier("mainDiscountPolicy")*/ @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
